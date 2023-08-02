a6d7092178eaffdf90e8ff359216ba974a99c7cb
maskibicki <128921693+maskibicki@users.noreply.github.com>
2023-08-02 10:48:30 -0500

    Add files via upload

--- pacman.py
+++ pacman.py
@@ -1,27 +1,99 @@
+
+
 import pygame
 import random
-from enum import Enum
-from collections import namedtuple
+x=50
+y=50
 
-pygame.init()
+background_colour = (0, 0, 0)
+displayHeight = 800
+displayWidth = 600
+width = 50
+height = 50
+pixel = 64
+vel = 5
+gostx = 400
+gosty = 300
 
-font = pygame.font.SysFont('calibri', 20)
+pinkwidth = 70
+bluewidth = 60
+display_surface = pygame.display.set_mode((displayHeight, displayWidth ))
 
-class Direction(Enum):
-    RIGHT = 1
-    LEFT = 2
-    UP = 3
-    DOWN = 4
+pygame.display.set_caption('Pacman')
+  
+
+image = pygame.image.load(r'man.png')
+redghost = pygame.image.load(r'redghost.png')
+pinkghost = pygame.image.load(r'pinkghost.png')
+orangeghost = pygame.image.load(r'orangeghost.png')
+blueghost = pygame.image.load(r'bluegost.png')
+pygame.display.flip()
+  
+
+
+scaled_image = pygame.transform.scale(image, (width, height))
+scaled_redghost = pygame.transform.scale(redghost, (width, height))
+scaled_pinkghost = pygame.transform.scale(pinkghost, (pinkwidth, height))
+scaled_orangeghost = pygame.transform.scale(orangeghost, (width, height))
+scaled_blueghost = pygame.transform.scale(blueghost, (bluewidth, height))
+running = True
+
+
+
+
+while running:
+    display_surface.fill(background_colour)
+    display_surface.blit(scaled_redghost, (gostx, gosty))
+    display_surface.blit(scaled_pinkghost, (gostx, gosty - 60))
+    display_surface.blit(scaled_orangeghost, (gostx - 60, gosty))
+    display_surface.blit(scaled_blueghost, (gostx - 90, gosty - 60))
+    for event in pygame.event.get():
+        
     
-Point = namedtuple('Point', 'x, y')
+        if event.type == pygame.QUIT:
+            running = False
+    def player(x, y):
+        display_surface.blit(scaled_image, (x, y))
 
-WHITE = (255, 255, 255)
-RED = (248,143,147)
-GREEN = (186,217,181)
-FONT = (66,12,20)
-BACK = (239,247,207)
+    circle = pygame.draw.circle(display_surface, (254, 255, 247),
+                      [400, 300], 15, 0)
+    line = pygame.draw.line(display_surface, (0, 0, 100),
+                 [100, 300],
+                 [500, 300], 5),
 
-BLOCK_SIZE = 20
-SPEED = 15
+    keys = pygame.key.get_pressed()
 
-class Pacman:
+
+  
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < displayHeight - vel - 50:
+        x += vel
+
+    if keys[pygame.K_UP] and y > vel:
+        y -= vel
+
+    if keys[pygame.K_DOWN] and y < displayWidth - vel - 50:
+        y += vel
+
+    keys = pygame.key.get_pressed()
+
+
+  
+    if keys[pygame.K_a] and gostx > vel and gostx - 60 > vel and gostx - 90 > vel:
+        gostx -= vel
+
+    if keys[pygame.K_d] and gostx < displayHeight - vel - 50 and gostx - 60 < displayHeight - vel - 50 and gostx - 90 < displayHeight - vel - 50:
+        gostx += vel
+
+    if keys[pygame.K_w] and gosty > vel and gosty - 60 > vel:
+        gosty -= vel
+
+    if keys[pygame.K_s] and gosty < displayWidth - vel - 50 and gosty - 60 < displayWidth - vel - 50:
+        gosty += vel
+
+
+    display_surface.blit(scaled_image, (x, y))  
+    pygame.display.update() 
+    
